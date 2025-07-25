708e35313378c9113a83c2de12e8cf9f3dc8f375
Leonel Perez <90221948+leps1998@users.noreply.github.com>
2025-07-25 20:46:52 -0300

    Update Program.cs

--- BirthdayRoomsBackend/Program.cs
+++ BirthdayRoomsBackend/Program.cs
@@ -20,11 +20,8 @@ builder.Services.AddScoped<IBookingService, BookingService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
